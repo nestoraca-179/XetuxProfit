29b589753f6bf99a9b062064249575fef3d7784a
alejandro <nestoraca.179@gmail.com>
2025-02-22 15:23:38 -0400

    carpeta logs

--- Controller/LogController.cs
+++ Controller/LogController.cs
@@ -7,7 +7,11 @@ namespace XetuxProfit.Controller
 	{
 		public static void WriteLog(string type, string msg)
 		{
-			string file = $"logs_{DateTime.Now:ddMMyyyy}.txt";
+			string file = $"logs\\logs_{DateTime.Now:ddMMyyyy}.txt";
+
+			if (!Directory.Exists("logs"))
+				Directory.CreateDirectory("logs");
+
 			using (StreamWriter writer = new StreamWriter(file, true)) // true para append
 			{
 				if (type != "LINE")
